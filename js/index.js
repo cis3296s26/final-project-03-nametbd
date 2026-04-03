25f4d2a54ff9e1e9ca87970c90fd3252d8314cd4
MarcoGiven <marcogiven15@gmail.com>
2026-04-03 19:48:46 -0400

    revamped login page + added responsive js to handle register page

--- js/index.js
+++ js/index.js
@@ -124,7 +124,70 @@ document.addEventListener('DOMContentLoaded', () =>{
             // TBD
         }
     }
+
 });
 
+// FOR PASSWORD SHOW/HIDE
+function togglePassword() {
+    const passwordInput = document.getElementById("password")
+    const passwordInputReg = document.getElementById("reg-password");
+    const eyeIcon = document.querySelector(".eye-icon")
+
+    // Fix for register AND sign in 
+    if(passwordInput.type === "password" || passwordInputReg.type === "password"){
+        passwordInput.type = "text"
+        passwordInputReg.type = "text"
+        eyeIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>'
+    } else {
+        passwordInput.type = "password"
+        passwordInputReg.type = "password"
+        eyeIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>'
+    }
+}
 
 
+
+
+// FOR CHANGING SCREEN FROM REGISTER TO SIGN IN + RESPONSIVE CARD RESIZING
+const formContainer = document.querySelector(".form-container");
+const loginContainer = document.querySelector(".login-container");
+const registerContainer = document.querySelector(".register-container");
+
+const loginBtn = document.querySelector("#login-btn");
+const registerBtn = document.querySelector("#register-btn")
+
+const loginForm = document.querySelector(".login-container");
+const registerForm = document.querySelector(".register-container");
+
+const fluid = document.querySelector("#fluid");
+
+function viewLogin() {
+    loginForm.style.left = "0";
+    registerForm.style.left = "100%";
+
+    formContainer.classList.remove("register-mode");
+
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+
+    fluid.classList.add("fluid-animate");
+}
+
+function viewRegister() {
+    loginForm.style.left = "-100%";
+    registerForm.style.left = "0";
+
+    formContainer.classList.add("register-mode");
+
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 1;
+
+    fluid.classList.add("fluid-animate");
+}
+
+registerBtn.addEventListener('click', viewRegister);
+loginBtn.addEventListener('click', viewLogin);
+
+fluid.addEventListener('animationend', () => {
+    fluid.classList.remove("fluid-animate");
+})
